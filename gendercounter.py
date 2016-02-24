f71ffbca7b39512f92796e4c21225819503aa23c
intensifier <christopher.kullenberg@gmail.com>
2016-02-24 13:53:57 +0100

    added pronoun support

--- gendercounter.py
+++ gendercounter.py
@@ -1,4 +1,5 @@
 from sys import argv
+import re
 
 script, filename = argv
 
@@ -37,15 +38,73 @@ def raknakvinnor():
 def raknamaen():
     maencounter = 0
     maennamnfrekvens = {}
-    for t in text:
-        if t in maendict:
-            maennamnfrekvens[t] = maendict[t]
+    for t2 in text:
+        if t2 in maendict:
+            maennamnfrekvens[t2] = maendict[t2]
             maencounter += 1
     return(maennamnfrekvens, maencounter)
+
+def raknahen():
+    hen = 0
+    henom = 0
+    hens = 0
+    for t3 in text:
+        henregexp = re.findall(r'\bhen\b', t3, flags = re.IGNORECASE)
+        henomregexp = re.findall(r'\bhenom\b', t3, flags = re.IGNORECASE)
+        hensregexp = re.findall(r'\bhens\b', t3, flags = re.IGNORECASE)
+        if henregexp:
+            hen += 1
+        elif henomregexp:
+            henom += 1
+        elif hensregexp:
+            hens += 1
+        else:
+            continue
+    return(hen, henom, hens)
+
+def raknahon():
+    hon = 0
+    henne = 0
+    hennes = 0
+    for t4 in text:
+        honregexp = re.findall(r'\bhon\b', t4, flags = re.IGNORECASE)
+        henneregexp = re.findall(r'\bhenne\b', t4, flags = re.IGNORECASE)
+        hennesregexp = re.findall(r'\bhennes\b', t4, flags = re.IGNORECASE)
+        if honregexp:
+            hon += 1
+        elif henneregexp:
+            henne += 1
+        elif hennesregexp:
+            hennes += 1
+        else:
+            continue
+    return(hon, henne, hennes)
+
+def raknahan():
+    han = 0
+    honom = 0
+    hans = 0
+    for t5 in text:
+        hanregexp = re.findall(r'\bhan\b', t5, flags = re.IGNORECASE)
+        honomregexp = re.findall(r'\bhonom\b', t5, flags = re.IGNORECASE)
+        hansregexp = re.findall(r'\bhans\b', t5, flags = re.IGNORECASE)
+        if hanregexp:
+            han += 1
+        elif honomregexp:
+            honom += 1
+        elif hansregexp:
+            hans += 1
+        else:
+            continue
+    return(han, honom, hans)
+
 
 #Launch the functions
 kvinnorslutresultat = raknakvinnor()
 maenslutresultat = raknamaen()
+henslutresultat = raknahen()
+honslutresultat = raknahon()
+hanslutresultat = raknahan()
 
 #Print the names
 for keys, values in kvinnorslutresultat[0].items():
@@ -56,6 +115,9 @@ for keys, values in maenslutresultat[0].items():
 #Print again at bottom of screeen
 print("Det finns " + str(len(kvinnorslutresultat[0])) + " unika kvinnonamn i texten.")
 print("Det finns " + str(len(maenslutresultat[0])) + " unika mansnamn i texten.")
-
 print("Absoluta tal kvinnor: " + str(kvinnorslutresultat[1]))
 print("Absoluta tal män: " + str(maenslutresultat[1]))
+print("Antalet hen, henom, hens : " + str(henslutresultat))
+print("Antalet hon, henne, hennes : " + str(honslutresultat))
+print("Antalet han*, honom, hans* : " + str(hanslutresultat))
+print('Obs: "Han" och "Hans" kan också vara manliga förnamn.')
